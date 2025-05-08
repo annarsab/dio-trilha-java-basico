d466c660370f75fff5ed6bf885cee0eacdd98b51
Anna Paula <bueno.annarsa@gmail.com>
2025-05-08 13:23:43 -0300

    Continuação da aula sobre operadores.

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,16 +1,41 @@
 public class Operadores {
   public static void main(String[] args) {
-    int numero = 5;
+    int a, b;
+    a = 6;
+    b = 6;
+    String resultado = a==b ? "verdadeiro" : "falso";
+    // operador ternario não se limita à String
+
+    System.out.println(resultado);
+    /*EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE */
+    /*if (a==b){
+      resultado = ("Verdadeiro.");      
+    } else {
+      resultado = ("Falso.");
+    }   
+    
+    System.out.println(resultado);*/
+    /*int numero = 5;
 
     numero = - numero; 
     System.out.println(numero);
 
     numero = numero * -1;
-    System.out.println(numero);
-    
-    
-  
-  
+    System.out.println(numero);*/
+
+    //repetição para incrementar o valor em uma determinada concepção de novos valores que vamos querer reatribuir
+
+    /*int numero = 5;
+    //++numero;
+    System.out.println(++ numero);
+    System.out.println(numero);*/
+
+    /*boolean variavel = true; // ou true
+
+    //quero determinar que essa variável é falsa
+
+    System.out.println(!variavel); //na memória, mas a variável original continua sendo true - simbolo de exclamação é NEGAÇÃO*/
+ 
   }
 
   
